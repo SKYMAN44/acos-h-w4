c4f8c581875b46929cf37bd224a3a2768632a3b7
Dmitrii Sokolov <55272093+SKYMAN44@users.noreply.github.com>
2021-01-31 17:57:09 +0300

    Update ex5.asm

--- ex5.asm
+++ ex5.asm
@@ -14,3 +14,46 @@ main:
      	li a7, 5
 	ecall
 	mv t1, a0
+	
+	#t1 < t0
+	blt t1, t0, 1branch
+	
+	#t0 < t1
+	blt t0, t1, 2branch
+	
+1branch:
+	
+	#print smallest
+	mv a0, t1
+	li a7, 1
+	ecall
+	la a0, space
+	li a7, 4
+	ecall
+	
+	#print largest
+	mv a0, t0
+	li a7, 1
+	ecall
+	la a0, space
+	li a7, 4
+	ecall
+	
+2branch:
+
+	#print smallest
+	mv a0, t0
+	li a7, 1
+	ecall
+	la a0, space
+	li a7, 4
+	ecall
+	
+	#print largest
+	mv a0, t1
+	li a7, 1
+	ecall
+	la a0, space
+	li a7, 4
+	ecall
+	
