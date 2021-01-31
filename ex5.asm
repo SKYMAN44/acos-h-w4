a56cadc36a4a8d4eaaa3c9eaca6bc87c77a8190a
Dmitrii Sokolov <55272093+SKYMAN44@users.noreply.github.com>
2021-01-31 17:45:14 +0300

    Update ex5.asm

--- ex5.asm
+++ ex5.asm
@@ -1,1 +1,16 @@
+	    .data
+space:
+	   .string "\n"
+	
+	    .text
+main:
 
+	  # x: t0 = readInt()
+     	li a7, 5
+	    ecall
+	    mv t0, a0
+      
+    # y: t0 = readInt()
+     	li a7, 5
+	    ecall
+	    mv t1, a0
