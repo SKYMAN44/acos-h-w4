ab71425f9cbecaf83bb86a50a3a429208c205dfa
Dmitrii Sokolov <55272093+SKYMAN44@users.noreply.github.com>
2021-01-31 22:28:57 +0300

    Update ex3.asm

--- ex3.asm
+++ ex3.asm
@@ -1,11 +1,25 @@
-  .text
-main:
-  mv t0, t1
-  li t0, 0x12345678
-  li t0, 16
-  b  label
-  j  label
-  la t0, label
-  
-label:
-      #labls
+# mv t0, t1
+#
+# add t0, zero, t1    
+# or addi t0, t1, 0   instructions used to replace
+
+# li t0, 0x12345678
+#
+# addi              instructions used to replace
+
+# li t0, 16
+#
+# addi t0, zero, 16     instructions used to replace
+
+# b label
+#
+# beq zero, zero, label     instructions used to replace
+
+# j label
+#
+# jal zero, label       instructions used to replace
+
+# la t0, label
+#
+# addi t0, t0, label    instructions used to replace
+# or auipc t0, label
